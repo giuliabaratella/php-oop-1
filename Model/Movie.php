85065f105d0c8064b146b894bc554be6a416bb4a
giuliabaratella <giuliabaratella7@gmail.com>
2023-12-04 15:15:35 +0100

    added class movie + constructor function

--- Model/Movie.php
+++ Model/Movie.php
@@ -1,3 +1,23 @@
 <?php
 
+
+class movie
+{
+    public int $id;
+    public string $title;
+    public string $overview;
+    public float $vote_average;
+    private string $poster_path;
+
+    function __construct($id, $title, $overview, $vote, $image)
+    {
+        $this->id = $id;
+        $this->title = $title;
+        $this->overview = $overview;
+        $this->vote_average = $vote;
+        $this->poster_path = $image;
+    }
+}
+
+
 ?>
